60ec8e6168fbe508e79666fd07433efcb8f8ea70
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-07-17 08:45:56 +0100

    save progress

--- database/migrations/20190716124539-create-status.js
+++ database/migrations/20190716124539-create-status.js
@@ -1,27 +1,29 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('statuses', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('statuses');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('statuses', {
+            id: {
+                // type: DataTypes.UUID,
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: {
+                type: Sequelize.STRING,
+                unique: true,
+            },
+            state: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
+            parent: {
+                type: Sequelize.INTEGER
+            },
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('statuses');
+    }
 };
+
